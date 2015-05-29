16cb83fab256d63e5155b56fcb7504ebafc75297
iku000888 <kanumaiku@gmail.com>
2015-05-29 09:18:26 +0900

    supply more comment pertaining to not running when there is a live Libre Office instance, and a formatting fix.

--- scripts/fodp2odp.sh
+++ scripts/fodp2odp.sh
@@ -1,9 +1,16 @@
 #a script to convert foedp file format to odp file format.
-#fodp file format is human readalble, so better for version controll.
-#odp is the binary counterpart, which is used for uploading slides to
-#slideshare.com
+#fodp file format is human readalble,
+#so the blind assumption is that its better for
+#version controll.
+
+#odp is the binary counterpart, which is
+#used for uploading slides toslideshare.com
 
 #$1 is the fodp file to be converted.
 #Invoke by pointing to the source fodp file
-#./fodp2odp ../Gitstandalone.fodp 
+#./fodp2odp ../Gitstandalone.fodp
+
+#Note that this will fail miserably if there
+#is a currently running instance of Libre Office. Shame.
+#Just ensure all Libre office instances are closed...
 soffice --headless --convert-to odp: $1
